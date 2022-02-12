06cc3738238d4ee607f3ed09611516c81d61f628
MonkeyOnFire <36528157+MonkeyOnFire@users.noreply.github.com>
2022-02-12 13:06:23 +0800

    。

---
+++ main/Tools/down_youtube.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+
+yt = YouTube(url='https://www.youtube.com/watch?v=V4ScM3uU1Gg&list=PLVVvqw65v_JaSwErTsH4Q03Rqo-PZv1P6&index=26')#,proxies={'http':'192.168.0.109:10809'})
+#yt.title
+#yt.thumbnail_url
+#yt.streams.all()
+stream = yt.streams.first()
+#stream
+stream.download('C:\\Users\\aaa31\\Desktop\\dw')
